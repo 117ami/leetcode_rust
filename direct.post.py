7ad55c59e35cc22dafb0a685f74bc58a8a0e7d29
117ami <ssruoz@gmail.com>
2020-03-21 16:15:58 +0800

    p941-M valid mountain array [Rust]

---
+++ direct.post.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3
+import sys 
+import os  
+
+if len(sys.argv) == 2 or sys.argv[1] == "xiu":
+    print("🐬🐬🐬 Submint through account : XIU")
+    os.system("cp /Users/alpha/.lc/xiu.json /Users/alpha/.lc/leetcode/user.json")
+elif sys.argv[1] == '117':
+    print("🔥🔥🔥 Submint through account : 117")
+    os.system("cp /Users/alpha/.lc/firefly.json /Users/alpha/.lc/leetcode/user.json")
+else:
+    print("Either xiu or 117")
+    exit(0)
+
+if sys.argv[-1].endswith('rs'):
+    print("Submitting Rust solution")
+    f = open('question.rs', 'r').read().__str__().replace('pub struct ', '// pub struct')
+    with open(sys.argv[-1], 'w') as wh:
+        wh.write(f)
+
+os.system('leetcode submit {}'.format(sys.argv[-1]))
+
+
--- main.rs
+++ main.rs
@@ -2,8 +2,9 @@
 mod question; 
 
 fn main(){
-	let n = 2; let m = 3; 
-	let ids = vec![vec![0, 1], vec![1, 1]];
-	println!("{:?}", question::Solution::odd_cells(n, m, ids));
+	// let m = vec![0,3,3];
+	let m = vec![0,1,2,3,4,5,6,7,8,9,8];
+	// let m = vec![9,8,7,6,5,4,3,2,1,0];
+	println!("{:?}", question::Solution::valid_mountain_array(m));
 }
 
