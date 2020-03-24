aee27ccef3940abe87d58d102fe788875416bd2b
117ami <ssruoz@gmail.com>
2020-03-24 10:16:34 +0800

    p1385-E find the distance value between two arrays

--- main.rs
+++ main.rs
@@ -2,6 +2,8 @@
 mod question; 
 
 fn main(){
-	println!("{:?}", question::Solution::get_kth(1, 1000, 777));
+	let arr1 = vec![4,5,8]; 
+	let arr2 = vec![10,9,1,8];
+	println!("{:?}", question::Solution::find_the_distance_value(arr1, arr2, 2));
 }
 
